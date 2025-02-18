0fa3582b9cdace9b8a949f96299f0be5289c8783
LewisLee26 <lewis.patrick.lee@gmail.com>
2025-02-18 15:53:17 +0000

    Cleaned up file and minor adjustments

--- scripts/experiments/hook_vision_test.py
+++ scripts/experiments/hook_vision_test.py
@@ -6,16 +6,13 @@ import image
 torch.manual_seed(0)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# model = models.googlenet(weights=models.GoogLeNet_Weights.IMAGENET1K_V1)
-# model = models.vit_l_16(weights=models.ViT_L_16_Weights.IMAGENET1K_V1)
-# model = models.swin_t(weights=models.Swin_T_Weights.IMAGENET1K_V1)
 model = models.swin_b(weights=models.Swin_B_Weights.IMAGENET1K_V1)
 
 params, image_f = image.image(224, fft=True, decorrelate=True, device=device)
 
 print(layer_names(model))
 
-hook = hook_specific_layer(model, "features.3.0.mlp")
+hook = hook_specific_layer(model, "features.1.0.mlp.2")
 model(image_f())
 
-print(hook.features)
+print(hook.features.shape)
